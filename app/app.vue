801683e1ce5b8d487b6b5f0aed80bea512c25e87
muhammadibnmurod <220481@centralasian.uz>
2025-09-09 21:59:27 +0500

    Add TypeScript configuration file for Nuxt project

---
+++ app/app.vue
@@ -0,0 +1,43 @@
+<script>
+import {defineNuxtComponent} from "#app";
+
+export default defineNuxtComponent({
+  data: () => ({
+    todoList: []
+  }),
+  methods: {
+    fetchToDoList() {
+      fetch('https://jsonplaceholder.typicode.com/todos/')
+          .then(response => response.json())
+          .then(json => {
+            this.todoList = json
+          })
+    }
+  }
+})
+
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer/>
+    <img src="/todo.jpg" alt="">
+    <p>
+      Photo by <a
+        href="https://unsplash.com/@glenncarstenspeters?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Glenn
+      Carstens-Peters</a> on <a
+        href="https://unsplash.com/photos/person-writing-bucket-list-on-book-RLw-UC03Gwc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+    </p>
+    <h1>Hello World</h1>
+    <button @click="fetchToDoList">Fetch Data</button>
+    <pre>
+          {{ todoList }}
+        </pre>
+    <ul>
+      <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
+        <input type="checkbox" :checked="todo.completed"/> {{ todo.title }}
+      </li>
+    </ul>
+  </div>
+</template>
